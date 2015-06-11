844acbffeec7c2953a6aa310e7611f711eba0dee
Nils Van Geele <nils@nvgeele.be>
2015-06-11 22:23:00 +0200

    JIT can enter for applications

--- slipy/interpreter.py
+++ slipy/interpreter.py
@@ -1,14 +1,15 @@
+from slipy.AST import Application
 from slipy.continuation import empty_continuation
 from slipy.environment import Env
 from slipy.exceptions import EvaluationFinished
 from rpython.rlib import jit
 
 
-def get_printable_location(ast):
+def get_printable_location(ast):  # , prev):
     return ast.to_string()
 
 
-driver = jit.JitDriver(reds=["env", "cont"], greens=["ast"],
+driver = jit.JitDriver(reds=["env", "cont"], greens=["ast"],  # , "prev"],
                        get_printable_location=get_printable_location)
 
 
@@ -26,10 +27,15 @@ def return_value_direct(value, env, cont):
 
 def _interpret(ast, env, cont):
     # from slipy.util import write
+    prev = ast
     while True:
-        driver.jit_merge_point(ast=ast, env=env, cont=cont)
+        driver.jit_merge_point(ast=ast,  # prev=prev,
+                               env=env, cont=cont)
         # write(str(cont.depth())+", ")
         ast, env, cont = ast.eval(env, cont)
+        if isinstance(ast, Application):
+            driver.can_enter_jit(ast=ast,  # prev=prev,
+                                 env=env, cont=cont)
 
 
 def interpret_with_env(ast, env):
