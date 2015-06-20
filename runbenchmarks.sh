928ae7fe8cfda7775fcd03655398dfbdbbf950f6
Nils Van Geele <nils@nvgeele.be>
2015-06-21 01:44:54 +0200

    Changed runbenchmarks to truncate some lines

--- runbenchmarks.sh
+++ runbenchmarks.sh
@@ -3,6 +3,6 @@
 for f in ./benchmarks/*.scm
 do
     echo $f
-    ./targetslipy-c $f
-    ../rpislip/slip -l $f
+    ../rpislip/slip -l $f | tail -n +3 | head -n 20 > _tmpA
+    ./targetslipy-c $f | tail -n +2 | head -n 20 > _tmpB
 done
